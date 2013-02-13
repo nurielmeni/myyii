694d0cb61c25159d2f0aad590e157454fb6afea2
02865824 <02865824@N73495.bezeq.com>
2013-02-14 01:50:58 +0200

    Added column yeshuv id Screen

--- protected/models/Screen.php
+++ protected/models/Screen.php
@@ -7,6 +7,7 @@
  * @property integer $id
  * @property string $name
  * @property string $description
+ * @property integer $yeshuv_id
  * @property integer $monitor_id
  * @property integer $client_id
  * @property string $create_time
@@ -48,12 +49,12 @@ class Screen extends BillboardActiveRecord
 		// will receive user inputs.
 		return array(
 			array('name', 'required'),
-			array('monitor_id, client_id', 'numerical', 'integerOnly'=>true),
+			array('monitor_id, client_id, $yeshuv_id', 'numerical', 'integerOnly'=>true),
 			array('name', 'length', 'max'=>128),
 			array('description', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, name, description, monitor_id, client_id, create_time, create_user_id, update_time, update_user_id', 'safe', 'on'=>'search'),
+			array('id, name, description, monitor_id, client_id, $yeshuv_id, create_time, create_user_id, update_time, update_user_id', 'safe', 'on'=>'search'),
                         array('name, monitor_id, client_id', 'required')
 		);
 	}
@@ -83,7 +84,8 @@ class Screen extends BillboardActiveRecord
 			'description' => 'תיאור',
 			'monitor_id' => 'מסך',
 			'client_id' => 'מחשב',
-			'create_time' => 'Create Time',
+                        '$yeshuv_id' => 'ישוב',
+                        'create_time' => 'Create Time',
 			'create_user_id' => 'Create User',
 			'update_time' => 'Update Time',
 			'update_user_id' => 'Update User',
@@ -106,6 +108,7 @@ class Screen extends BillboardActiveRecord
 		$criteria->compare('description',$this->description,true);
 		$criteria->compare('monitor_id',$this->monitor_id);
 		$criteria->compare('client_id',$this->client_id);
+		$criteria->compare('yeshuv_id',$this->yeshuv_id);
 		$criteria->compare('create_time',$this->create_time,true);
 		$criteria->compare('create_user_id',$this->create_user_id);
 		$criteria->compare('update_time',$this->update_time,true);
