05c5a0408ff8c9e6ca5928a69f84cb32495c850b
Meni <Meni@Meni>
2014-06-30 11:30:01 +0300

    time date and whether

---
+++ js/date_time.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function date_time(id)
+{
+        date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('ינואר', 'פברואר', 'מרס', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ''+days[day]+' '+d+' ב'+months[month]+' '+year+'\n'+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("'+id+'");','1000');
+        return true;
+}
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,14 +8,18 @@ $this->layout='//layouts/billboard';
 
     <div class="row-fluid" id="TopRow">
 	<div class="span6 myyiiBorder" id="RightColumn">
-            <div class="myyiiBorder" id="digiclock">
+            <div id="date_time"></div>
+            <div>
+                <a href="http://www.accuweather.com/he/il/bat-yam/212477/weather-forecast/212477" class="aw-widget-legal">
+                </a><div id="awcc1403769386036" class="aw-widget-current"  data-locationkey="" data-unit="c" data-language="he" data-useip="true" data-uid="awcc1403769386036"></div><script type="text/javascript" src="http://oap.accuweather.com/launch.js"></script>
             </div>	
             <div class="myyiiBorder" id="Adds1">
-                <iframe src="banner/251517d827603199f6ddcd02a83b5425756b1a80f4c0/251517d827603199f6ddcd02a83b5425756b1a80f4c0.html" width="350" height="400" frameborder="0" scrolling="no" allowTransparency="true"></iframe>
-            </div>	
+                <iframe id="html5d8deb0eec9fed3f2af208bd31b6b834104c796bd0fce" src="banner/d8deb0eec9fed3f2af208bd31b6b834104c796bd0fce/d8deb0eec9fed3f2af208bd31b6b834104c796bd0fce.html" width="100%" height="460" frameborder="0" scrolling="no" allowTransparency="true"></iframe>
+             </div>	
  	</div>
 	<div class="span6 myyiiBorder" id="LeftColumn">
             <div class="myyiiBorder" id="Logo">
+                <img src="<?php echo Yii::app()->baseUrl.'/images/Logo.PNG'; ?>" />
             </div>	
             <div class="myyiiBorder" id="Billboard">
 		<div id="banner-fade">
@@ -36,55 +40,48 @@ $this->layout='//layouts/billboard';
             </div>	
     </div>
 
-	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jdigiclock/lib/jquery.jdigiclock.js"></script>
-
+	
 	<script type="text/javascript"> <!-- DOM Ready functions (jdigiclock, 
 	    $(document).ready(function() {
-	        $('#digiclock').jdigiclock({
-	            // Configuration goes here
-	            clockImagesPath: 'js/jdigiclock/images/clock/',
-                    weatherImagesPath: 'js/jdigiclock/images/weather/',
-                    lang: 'he',
-                    am_pm: false,
-                    weatherLocationCode: 'MEA|IL|IS005|HOLON',
-                    weatherMetric: 'C'                             
-	        });	  
-			
-                // Billboard slider
+	        // Billboard slider
 	        $('#banner-fade').bjqs({
                     'showcontrols' : false,
                     'animduration' : '1250', // how fast the animation are
                     'animspeed' : '6500', // the delay between each slide
                     responsive : true
                 });      
+                
+                // Date and Time
+                date_time('date_time');
 	    });	   
 	</script>
 	
-	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/sagscroller.js"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/sagscroller.js"></script>
 
-	<!-- Include the plugin *after* the jQuery library -->
-	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/bjqs.js"></script>
+    <!-- Include the plugin *after* the jQuery library -->
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/bjqs.js"></script>
+    
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/date_time.js"></script>
 
-	<!-- Include the basic styles -->
-	<link type="text/css" rel="Stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bjqs.css" charset="UTF-8" />
+    <!-- Include the basic styles -->
+    <link type="text/css" rel="Stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bjqs.css" charset="UTF-8" />
 
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-	<script type="text/javascript"> <!-- Ynet Widget
-		var ynetNewsScroller=new sagscroller({
-			id:'ynetNewsScroller',
-			mode: 'auto',
-			rssdata:{
-				feeds: [
-					['Ynet', 'http://www.ynet.co.il/Integration/StoryRss2.xml']
-				],
-				linktarget: '_new',
-				displayoptions: 'description',
-				groupbylabel: false,
-				entries: 100 //<--no comma following last option
-			},
-			refreshsecs: 360,
-			pause: 10000,
-			animatespeed: 3500 //<--no comma following last option
-		});
-	</script>
-	
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript"> <!-- Ynet Widget
+            var ynetNewsScroller=new sagscroller({
+                    id:'ynetNewsScroller',
+                    mode: 'auto',
+                    rssdata:{
+                            feeds: [
+                                    ['Ynet', 'http://www.ynet.co.il/Integration/StoryRss2.xml']
+                            ],
+                            linktarget: '_new',
+                            displayoptions: 'description',
+                            groupbylabel: false,
+                            entries: 100 //<--no comma following last option
+                    },
+                    refreshsecs: 360,
+                    pause: 10000,
+                    animatespeed: 3500 //<--no comma following last option
+            });
+    </script>
